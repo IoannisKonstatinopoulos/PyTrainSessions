4da3afc47a185c2b2c68d5d6d767d6af6e955124
IoannisKonstatinopoulos <74180296+IoannisKonstatinopoulos@users.noreply.github.com>
2021-12-23 23:50:59 +0200

    Update 1st_assignment.py

--- 1st_assignment.py
+++ 1st_assignment.py
@@ -1,36 +1,36 @@
 #Exercise #2
 
 print("Exercise #1\n")
-list = ["pizza", "loukoumades", "melomakarona", "kourampiedes", "tzaziki", "paidakia"]
+listFoods = ["pizza", "loukoumades", "melomakarona", "kourampiedes", "tzaziki", "paidakia"]
 items = 0
-for food in list:
+for food in listFoods:
     print(food)
     items += 1
-print("Total number of items = ",items, "\n")
+print("Total number of items = ",items)
 
 #Exercise #2
-print("Exercise #2\n")
+print("\nExercise #2\n")
 
 letters = ['a','e','i','o','u']
 souma_letters = [0,0,0,0,0]
-for food in list:
+for food in listFoods:
     for letter in range(len(souma_letters)):
         souma_letters[letter] += food.count(letters[letter])
 
-for letter in range(len(souma_letters)):
+for letter in range(len(letters)):
     print("The letter ",letters[letter]," appears ",souma_letters[letter],"times")
 
 #Exercise #3
 
-print("\n#Exercise #3\n")
-def reverse_item(x):
-    return x[::-1]
-for food in list:
-    print(reverse_item(food))
+print("\nExercise #3\n")
+
+listFoodsReversed = [i[::-1] for i in listFoods]
+for reversedFood in listFoodsReversed:
+    print(reversedFood)
 
 #Exercise #4
 
-print("\n#Exercise #4\n")
+print("\nExercise #4\n")
 diction = {}
 for key in range(len(letters)):
     diction[letters[key]] = souma_letters[key]
